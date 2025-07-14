45f0e6f3333e1c6891cbf05bff3d2115d8979769
adii <114977269+ajhasbeensummoned@users.noreply.github.com>
2025-07-15 00:34:53 +0530

    small commit to __init__.py

--- lastmp3/__init__.py
+++ lastmp3/__init__.py
@@ -5,7 +5,7 @@ A user-friendly Python tool that downloads your most recent Last.fm tracks as hi
 with automatic metadata tagging.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .api import fetch_recent_track, tracklist, validate_api_key
 from .metadata import tag_mp3
